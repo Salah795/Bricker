e14a482b9d647c20925f8ef2c9d59312c4f62b63
Salah795 <salah.taher4@gmail.com>
2025-05-13 17:13:38 +0300

    working on DoubleBehaviorStrategy

--- src/bricker/brick_strategies/DoubleBehaviorStrategy.java
+++ src/bricker/brick_strategies/DoubleBehaviorStrategy.java
@@ -1,27 +1,54 @@
 package bricker.brick_strategies;
 
+import bricker.main.BrickerGameManger;
 import danogl.GameObject;
 
 import java.util.Random;
 
 public class DoubleBehaviorStrategy implements CollisionStrategy {
+    private static final int FIRST_POSSIBLE_RANDOM_INDEX = 6;
+    private static final int LAST_POSSIBLE_RANDOM_INDEX = 10;
+    private static final int POSSIBLE_STRATEGIES = 3;
+
     private CollisionStrategy[] collisionStrategies;
+    private BrickerGameManger brickerGameManger;
+    private CollisionStrategyFactory collisionStrategyFactory;
     private Random random;
+    private int filledStrategies;
 
-
-    public DoubleBehaviorStrategy() {
-        this.collisionStrategies = new CollisionStrategy[2];
+    public DoubleBehaviorStrategy(BrickerGameManger brickerGameManger) {
+        this.collisionStrategies = new CollisionStrategy[POSSIBLE_STRATEGIES];
+        this.filledStrategies = POSSIBLE_STRATEGIES - 1;
+        this.brickerGameManger = brickerGameManger;
+        this.collisionStrategyFactory = new CollisionStrategyFactory();
         this.random = new Random();
+        chooseStrategies();
     }
 
-    private void ChooseStrategies() {
-
+    public void chooseStrategies() {
+        for(int index = 0; index < POSSIBLE_STRATEGIES - 1; index++) {
+            int chosenIndex = this.random.nextInt(FIRST_POSSIBLE_RANDOM_INDEX,
+                    LAST_POSSIBLE_RANDOM_INDEX + 1);
+            this.collisionStrategies[index] = this.collisionStrategyFactory.buildCollisionStrategy(chosenIndex,
+                    brickerGameManger);
+            if(chosenIndex == LAST_POSSIBLE_RANDOM_INDEX) {
+                filledStrategies++;
+                chosenIndex = this.random.nextInt(FIRST_POSSIBLE_RANDOM_INDEX,
+                        LAST_POSSIBLE_RANDOM_INDEX);
+                this.collisionStrategies[index] = this.collisionStrategyFactory.buildCollisionStrategy(chosenIndex,
+                        brickerGameManger);
+                chosenIndex = this.random.nextInt(FIRST_POSSIBLE_RANDOM_INDEX,
+                        LAST_POSSIBLE_RANDOM_INDEX);
+                this.collisionStrategies[POSSIBLE_STRATEGIES - 1] = this.collisionStrategyFactory.
+                        buildCollisionStrategy(chosenIndex, brickerGameManger);
+            }
+        }
     }
 
     @Override
     public void onCollision(GameObject first, GameObject second) {
-        for(CollisionStrategy collisionStrategy : this.collisionStrategies) {
-            collisionStrategy.onCollision(first, second);
+        for(int index = 0; index < filledStrategies; index++) {
+            this.collisionStrategies[index].onCollision(first, second);
         }
     }
 }
